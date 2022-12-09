6af0ad5fda9dcaa6f398fefb8bcc8c9198264896
Saktakamol <118149608+Saktakamol@users.noreply.github.com>
2022-12-09 13:19:46 +0700

    Create provpn.php

---
+++ provpn.php
@@ -0,0 +1,6 @@
+{
+"Version_up09-12-2022 12:52:58": "1.0",
+"Link_zip":"https://github.com/Saktakamol/ling/blob/main/provpn.zip?raw=true",
+"Password": "090529",
+"Message": "●ควรอ่านรายละเอียดก่อนใช้งาน
+● ควรมีอินเทอร์เน็ตทุกครั้งที่อัปเดต!"}
