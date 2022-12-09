d04ad31709286aa6969ba476ffc6eeeed5c043e4
Saktakamol <118149608+Saktakamol@users.noreply.github.com>
2022-12-09 13:28:38 +0700

    Update provpn.php

--- provpn.php
+++ provpn.php
@@ -1,5 +1,5 @@
 {
-"Version_up09-12-2022 12:52:58": "1.0",
+"Version_update": "1.0",
 "Link_zip":"https://github.com/Saktakamol/ling/blob/main/provpn.zip?raw=true",
 "Password": "090529",
 "Message": "●ควรอ่านรายละเอียดก่อนใช้งาน
