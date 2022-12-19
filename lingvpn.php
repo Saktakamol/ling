44b243b5277f5f034f3f967468ca87f8e1d14226
Saktakamol <118149608+Saktakamol@users.noreply.github.com>
2022-12-19 09:15:40 +0700

    Update lingvpn.php

--- lingvpn.php
+++ lingvpn.php
@@ -1,24 +1,19 @@
-1.4
+1.5
 LING VPN 
-Update File 01-12-22 
+Update File 19-12-22 
 รายการเซิฟเวอร์ 
-AUS 5 SERVER 
-AUTO 10 SERVER
-CA 10 SERVER 
-SG-DIGITAL 10 SERVER 
-SG-EVM 10 SERVER 
-SG-GCORE 10 SERVER 
-SG-LABS 10 SERVER 
-SG-LINODE 10 SEVER
-SG-SECURE 10 SERVER 
-TH-CAT 10 SERVER 
-TH-GAMING 10 SERVER
-TH-IDC 10 SERVER 
-TH-READY 10 SERVER
+AUS 5 SERVER ใช้งานทั่วไป
+AUTO 10 SERVER ใช้งานทั่วไป
+CA 10 SERVER ใช้งานทั่วไป
+SG-DIGITAL 10 SERVER ใช้งานทั่วไป
+SG-EVM 10 SERVER ใช้งานทั่วไป
+TH-CAT 10 SERVER ใช้งานทั่วไป
+TH-GAMING 10 SERVER ใช้เล่นเกมส์เท่านั้น
+TH-IDC 10 SERVER ใช้งานทั่วไป
+TH-READY 10 SERVER ใช้งานทั่วไป
 VIP-CAT 4 SERVER เช่าเดือนละ30บาท 
-VIP-CSNE 3 SERVER
-VIP-TH 3 SERVER 
-VIP-SG 3 SERVER 
+VIP-TH 3 SERVER ใช้งานทั่วไป
+VIP-SG 3 SERVER ใช้งานทั่วไป
 กันรั่วซิมทรู *935*99# ฟรี 
 กันรั่วซิมดีแทค *117*30# 20บาท 
 กันรั่วซิมAIS *777*7067# 31บาท
